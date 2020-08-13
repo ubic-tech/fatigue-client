2d4e1f4e766e7d5e8105c8f75cba20e9d5729a0e
armnov <arm.nov@ya.ru>
2020-08-13 14:43:40 +0300

    Aggregator aggregates drivers

--- aggregator.py
+++ aggregator.py
@@ -7,6 +7,29 @@ class Aggregator:
         self.name = name
         self.id = _id
         self.port = port
+        self.drivers = {}
+
+    def add_driver(self, driver):
+        k, v = driver
+        self.drivers[k] = v
+
+    def set_fatigue(self, driver_id, val):
+        self.drivers[driver_id] = val
+
+    def set_last_hour(self, driver_id, val):
+        self.drivers[driver_id] = val
+
+    def set_last_quarters(self, driver_id, values):
+        self.drivers[driver_id] = [val for val in values[:4]]
+
+    def get_fatigue(self, driver_id,):
+        return self.drivers[driver_id].fatigue
+
+    def get_last_hour(self, driver_id,):
+        return self.drivers[driver_id].last_hour
+
+    def get_last_quarters(self, driver_id,):
+        return self.drivers[driver_id].last_quarters
 
     def run(self):
         handler = HandleRequests
