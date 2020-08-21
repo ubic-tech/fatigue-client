a72ecea5239eef23adf66e369d875443cd0388bc
armnov <arm.nov@ya.ru>
2020-08-21 16:15:18 +0300

    TMPC-22 data models for /v1/drivers/online/hourly

--- rest_models.py
+++ rest_models.py
@@ -2,6 +2,20 @@ from pydantic import BaseModel
 from typing import List
 
 
+class DriverData(BaseModel):
+    hash_id: str
+    shares: List[int]  # 1 or 4 values depending on request/response
+
+
+class DriversRequest(BaseModel):
+    timestamp: str
+    chain: str
+
+
+class DriversOnlineHourlyRequest(DriversRequest):
+    drivers: List[DriverData]
+
+
 class DriverFatigue(BaseModel):
     hash_id: str
     online: int
