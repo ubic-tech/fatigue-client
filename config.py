a764d8528d2e952c40497b2a24a187bedc4735bb
armnov <arm.nov@ya.ru>
2020-08-21 16:10:45 +0300

    ERROR, SUCCESS, driver samples

---
+++ config.py
@@ -0,0 +1,12 @@
+ERROR = {'code': "503", 'message': "NOT OK"}
+SUCCESS = {'code': "200", 'message': "OK"}
+
+
+DRIVERS_DATA = [
+    # name, license id
+    ("Rogue", 101),
+    ("Tough", 202),
+    ("Harsh", 303),
+    ("Rough", 404),
+    ("Rigid", 505),
+]
