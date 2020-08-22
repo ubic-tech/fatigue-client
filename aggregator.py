456f8fbfe13394e90a27dc913a33e2b9d4f17544
armnov <arm.nov@ya.ru>
2020-08-22 21:45:47 +0300

    aggrs iface added

--- aggregator.py
+++ aggregator.py
@@ -6,6 +6,11 @@ class Aggregator:
         self.name = name
         self.id = _id
         self.drivers = DriversDB()
+        self.aggregators = {}  # {"hash_id": "url", ...}
 
     def add_driver(self, full_name, license_id):
         self.drivers.add_driver(full_name, license_id)
+
+    def add_aggregator(self, hash_id, url):
+        if hash_id != self.id:
+            self.aggregators[hash_id] = url
