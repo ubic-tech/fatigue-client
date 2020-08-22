8e5ba7a3e19e7e6fe645e71dd199417bfdffafe7
armnov <arm.nov@ya.ru>
2020-08-22 21:45:09 +0300

    aggr data

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 ERROR = {'code': "503", 'message': "NOT OK"}
 SUCCESS = {'code': "200", 'message': "OK"}
 
+AGGREGATORS_DATA = {
+    "9600": "http://127.0.0.1:9600",
+    "19200": "http://127.0.0.1:19200",
+    "57600": "http://127.0.0.1:57600",
+}
 
 DRIVERS_DATA = [
     # name, license id
