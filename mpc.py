bcc0a7e08443b56a27e5db1ca4c723fb581b04da
armnov <arm.nov@ya.ru>
2020-08-23 18:37:36 +0300

    vars naming + comments

--- mpc.py
+++ mpc.py
@@ -12,37 +12,37 @@ def get_rand_pair(base: int) -> (int, int):
 
 
 def mpc_strategy(headers, request, route, aggregator, data_extractor):
-    try:
+    try:  # trying find 'myself' in the chain
         self_index = request.chain.index(aggregator.id)  # get index of aggr in chain
     except ValueError:
         return ERROR
 
-    try:
+    try:  # trying find next aggr in chain
         next_aggr_hash_id = request.chain[self_index + 1]
-    except IndexError:  # means this is the last aggregator in the chain
-        for i, driver in enumerate(request.drivers):
+    except IndexError:  # means 'me' is the last aggregator in the chain
+        for i, driver in enumerate(request.drivers):  # sum up 'my' shares to received ones
             driver_data = data_extractor(driver.hash_id, request.timestamp)
-            for j, d in enumerate(driver_data):
+            for j, d in enumerate(driver_data):  # 1 or 4 values in list are expected
                 request.drivers[i].shares[j] += d
         r = send(UBIC_URL + V1_SHARES, headers, data=dumps(request))  # simply send ubic our share summed with total
-        if r is None:
+        if r is None:  # handle errors
             pass
         return SUCCESS
 
-    ubic_shares = []
+    ubic_drivers_shares = []  # to be sent to UBIC
     next_aggr_url = get_endpoint_url_by_hash(next_aggr_hash_id,
                                              headers["X-Authorization"])
     for i, driver in enumerate(request.drivers):
-        ubic_part = DriverData()
-        ubic_part.hash_id = driver.hash_id
-        raw_driver_data = data_extractor(driver.hash_id, request.timestamp)
+        ubic_driver_shares = DriverData()  # to be appended to ubic_drivers_shares
+        ubic_driver_shares.hash_id = driver.hash_id
+        raw_driver_data = data_extractor(driver.hash_id, request.timestamp)  # value from driversDB: [sh, ...]
         for j, d in enumerate(raw_driver_data):
-            for_ubic, for_common = get_rand_pair(int(d))
-            request.drivers[i].shares[j] += for_common
-            ubic_part.shares.append(for_ubic)
-        ubic_shares.append(ubic_part)
+            for_ubic, for_common = get_rand_pair(int(d))  # for_ubic + for_common == d
+            request.drivers[i].shares[j] += for_common  # add 'my' share summed up with common
+            ubic_driver_shares.shares.append(for_ubic)  # keep all shares (looks like: [share,...]) of a driver
+        ubic_drivers_shares.append(ubic_driver_shares)
 
-    r = send(UBIC_URL + V1_SHARES, headers, data=dumps(ubic_shares))
+    r = send(UBIC_URL + V1_SHARES, headers, data=dumps(ubic_drivers_shares))
     if r is None:  # handle errors
         return ERROR
 
