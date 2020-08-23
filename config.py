586b99a4a25c94fbfb075821cb2a6a15fa646d97
armnov <arm.nov@ya.ru>
2020-08-23 14:14:35 +0300

    ubic routes

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 ERROR = {'code': "503", 'message': "NOT OK"}
 SUCCESS = {'code': "200", 'message': "OK"}
 
+UBIC_URL = "http://127.0.0.1:8888"
+V1_SHARES = "/v1/shares"
+V1_ENDPOINTS = "/v1/endpoints"
+
 AGGREGATORS_DATA = {
     "9600": "http://127.0.0.1:9600",
     "19200": "http://127.0.0.1:19200",
