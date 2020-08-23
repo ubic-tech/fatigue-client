6f92444fc9e73f03bf93bb178229e4d8cf065389
armnov <arm.nov@ya.ru>
2020-08-23 14:15:06 +0300

    drivers -> driversDB

--- aggregator.py
+++ aggregator.py
@@ -5,11 +5,11 @@ class Aggregator:
     def __init__(self, name, _id):
         self.name = name
         self.id = _id
-        self.drivers = DriversDB()
+        self.drivers_db = DriversDB()
         self.aggregators = {}  # {"hash_id": "url", ...}
 
     def add_driver(self, full_name, license_id):
-        self.drivers.add_driver(full_name, license_id)
+        self.drivers_db.add_driver(full_name, license_id)
 
     def add_aggregator(self, hash_id, url):
         if hash_id != self.id:
