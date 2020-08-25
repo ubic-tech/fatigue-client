fdfd16672d9138a66d3ee16c37bd3156ea9d4196
armnov <arm.nov@ya.ru>
2020-08-25 17:24:57 +0300

    request fixed

--- tests/curl.py
+++ tests/curl.py
@@ -1,4 +1,4 @@
-from aio_requests import get, post
+from utils import request, StatusError
 from tests.curl_data import *
 from json import dumps
 import asyncio
@@ -9,39 +9,48 @@ url = "http://127.0.0.1:8080"
 
 
 async def test_v1_health():
-    return await get(url + "/v1/health", headers=headers)
+    return await request(url + "/v1/health", headers=headers, method="get")
 
 
 async def test_drivers_fatigue():
-    return await post(url + "/v1/drivers/fatigue",
-                      headers=headers,
-                      data=dumps(drivers_fatigue_data))
+    return await request(url + "/v1/drivers/fatigue",
+                         headers=headers,
+                         data=dumps(drivers_fatigue_data))
 
 
 async def test_drivers_online_hour():
-    return await post(url + "/v1/drivers/online/hourly",
-                      headers=headers,
-                      data=dumps(drivers_online_hourly_request_data))
+    return await request(url + "/v1/drivers/online/hourly",
+                         headers=headers,
+                         data=dumps(drivers_online_hourly_request_data))
 
 
 async def test_drivers_online_quarter_hourly():
-    return await post(url + "/v1/drivers/online/quarter_hourly",
-                      headers=headers,
-                      data=dumps(drivers_online_quarter_hourly_request_data))
+    return await request(url + "/v1/drivers/online/quarter_hourly",
+                         headers=headers,
+                         data=dumps(drivers_online_quarter_hourly_request_data))
 
 
 async def test_drivers_on_order():
-    return await post(url + "/v1/drivers/on_order",
-                      headers=headers,
-                      data=dumps(drivers_on_order_data))
+    return await request(url + "/v1/drivers/on_order",
+                         headers=headers,
+                         data=dumps(drivers_on_order_data))
 
 
 async def all_tests():
-    print(await test_v1_health())
-    print(await test_drivers_fatigue())
-    print(await test_drivers_online_hour())
-    print(await test_drivers_online_quarter_hourly())
-    print(await test_drivers_on_order())
+    tests = [
+        test_v1_health,
+        test_drivers_fatigue,
+        test_drivers_online_hour,
+        test_drivers_online_quarter_hourly,
+        test_drivers_on_order,
+    ]
+
+    for test in tests:
+        try:
+            print(await test())
+        except StatusError:
+            print(test.__name__, ": unsuccessful request")
+
 
 if __name__ == "__main__":
     asyncio.run(all_tests())
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ class StatusError(Exception):
 
 async def request(url, *, method='post', expected_status=200, **kwargs):
     async with aiohttp.ClientSession() as session:
-        async with session.request(url, method, **kwargs) as resp:
+        async with session.request(url=url, method=method, **kwargs) as resp:
             if resp.status != expected_status:
                 raise StatusError(resp.status, await resp.text())
             return await resp.json()
