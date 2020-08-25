486bb181ccdcaf60123f1d8c061244c2f9113c40
armnov <arm.nov@ya.ru>
2020-08-25 16:56:18 +0300

    TMPC-19 configs shrinked

--- config.py
+++ config.py
@@ -1,21 +1,3 @@
-ERROR = {'code': "503", 'message': "NOT OK"}
-SUCCESS = {'code': "200", 'message': "OK"}
-
 UBIC_URL = "http://127.0.0.1:8888"
 V1_SHARES = "/v1/shares"
 V1_ENDPOINTS = "/v1/endpoints"
-
-AGGREGATORS_DATA = {
-    "9600": "http://127.0.0.1:9600",
-    "19200": "http://127.0.0.1:19200",
-    "57600": "http://127.0.0.1:57600",
-}
-
-DRIVERS_DATA = [
-    # name, license id
-    ("Rogue", 101),
-    ("Tough", 202),
-    ("Harsh", 303),
-    ("Rough", 404),
-    ("Rigid", 505),
-]
