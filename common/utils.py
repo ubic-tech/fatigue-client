7a8f5a86f320e23a293132a37f75076973369695
armnov <arm.nov@ya.ru>
2020-08-27 17:26:41 +0300

    timestamp_to_datetime

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,8 @@
 import aiohttp
+from re import search, findall, sub
 from config import AggregatorConfig
 from models.models import EndpointResponse
+from datetime import datetime, date, time
 
 
 class StatusError(Exception):
@@ -21,3 +23,24 @@ async def get_endpoint_url_by_hash(hash_id):
     if resp is None:
         pass  # todo: validate
     return EndpointResponse(**resp).endpoints[0].endpoint
+
+
+def timestamp_to_datetime(timestamp: str) -> datetime:
+    pattern = "{year}-{month}-{day}T{hour}:{minute}:{second}:{millis}Z"
+
+    timestamp = timestamp.replace(".", ":")  # !!! crutch !!!
+
+    # re stuff
+    regex = sub(r'{(.+?)}', r'(?P<_\1>.+)', pattern)
+    values = list(search(regex, timestamp).groups())
+    keys = findall(r'{(.+?)}', pattern)
+
+    #  values to int
+    for i in range(len(values)):
+        values[i] = int(values[i])
+
+    _dict = dict(zip(keys, values))
+    d = date(_dict["year"], _dict["month"], _dict["day"])
+    t = time(_dict["hour"], _dict["minute"], _dict["second"], _dict["millis"] * 1000)
+
+    return datetime.combine(d, t)
