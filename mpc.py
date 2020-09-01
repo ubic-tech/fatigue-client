01512f62eae41e445b999c017034d74f0fbf44cd
armnov <arm.nov@ya.ru>
2020-09-01 16:21:33 +0300

    TMPC-19 get_next_endpoint_hash_id extracted

--- mpc.py
+++ mpc.py
@@ -16,27 +16,53 @@ class OperationError(Exception):
     pass
 
 
-def get_next_endpoint(chain: List[str]):
-    return None
+def get_next_endpoint_hash_id(chain: List[str]) -> str:
+    try:  # trying find 'myself' in the chain
+        self_index = chain.index(AggrConf.AGGR_HASH_ID)  # get index of aggr in chain
+    except ValueError:
+        raise OperationError
+    try:  # trying find next aggr in chain
+        return chain[self_index + 1]
+    except IndexError:  # means 'me' is the last aggregator in the chain
+        return ""
 
 
 async def common_strategy(headers, req_body, route, data_extractor):
-    try:  # trying find 'myself' in the chain
-        self_index = req_body.chain.index(AggrConf.AGGR_HASH_ID)  # get index of aggr in chain
-    except ValueError:
-        # raise OperationError
-        pass
-
     ts = timestamp_to_datetime(req_body.timestamp)
 
     drivers_hash_ids = []
     for _d in req_body.drivers:
         drivers_hash_ids.append(_d.hash_id)
     self_data = data_extractor(ts, drivers_hash_ids)  # Mapping[DriverID, Share]
-    print(self_data)
-    try:  # trying find next aggr in chain
-        next_aggr_hash_id = req_body.chain[self_index + 1]
-    except IndexError:  # means 'me' is the last aggregator in the chain
+
+    print(self_data)  # DBG
+
+    if next_endpoint_hash_id := get_next_endpoint_hash_id(req_body.chain):
+        #next_endpoint_url = await get_endpoint_url_by_hash(next_endpoint_hash_id)  # request in advance
+        ubic_drivers_shares = []  # to be sent to UBIC
+        for i, driver in enumerate(req_body.drivers):
+            self_shares = self_data[driver.hash_id]
+            ubic_driver_data = DriverData(hash_id=driver.hash_id, shares=[])  # to be appended to ubic_drivers_shares
+            for j, share in enumerate(self_shares):
+                for_ubic, for_common = get_rand_pair(int(share))  # for_ubic + for_common == d
+                ubic_driver_data.shares.append(for_ubic)
+                req_body.drivers[i].shares[j] += for_common  # add 'my' share summed up with common
+            ubic_drivers_shares.append(ubic_driver_data)
+
+        print("ubic_drivers_shares", ubic_drivers_shares, "\n\n")  # DBG
+        print("req_body", req_body.drivers, "\n\n")  # DBG
+
+        #r = await request(AggrConf.UBIC_URL + AggrConf.SHARES_ROUTE,
+        #                  headers=headers,
+        #                  json=ubic_drivers_shares)
+        #if r is None:  # handle errors
+        #    pass
+
+        #r = await request(next_endpoint_url + route, headers=headers, json=req_body)
+        #if r is None:  # handle errors
+        #    # raise OperationError
+        #    pass
+    else:
         for i, driver_data in enumerate(req_body.drivers):  # sum up 'my' shares with received ones
             _id = driver_data.hash_id
             self_shares = self_data[_id]
@@ -47,29 +73,3 @@ async def common_strategy(headers, req_body, route, data_extractor):
         #if r is None:  # handle errors
         #    pass
         print("req_body", req_body.drivers, "\n\n")
-        return
-
-    #next_aggr_url = await get_endpoint_url_by_hash(next_aggr_hash_id)  # request in advance
-    ubic_drivers_shares = []  # to be sent to UBIC
-    for i, driver in enumerate(req_body.drivers):
-        self_shares = self_data[driver.hash_id]
-        ubic_driver_data = DriverData(hash_id=driver.hash_id, shares=[])  # to be appended to ubic_drivers_shares
-        for j, share in enumerate(self_shares):
-            for_ubic, for_common = get_rand_pair(int(share))  # for_ubic + for_common == d
-            ubic_driver_data.shares.append(for_ubic)
-            req_body.drivers[i].shares[j] += for_common  # add 'my' share summed up with common
-        ubic_drivers_shares.append(ubic_driver_data)
-
-    print("ubic_drivers_shares", ubic_drivers_shares, "\n\n")
-    print("req_body", req_body.drivers, "\n\n")
-"""
-    r = await request(AggrConf.UBIC_URL + AggrConf.SHARES_ROUTE,
-                      headers=headers,
-                      json=ubic_drivers_shares)
-    if r is None:  # handle errors
-        pass
-
-    r = await request(next_aggr_url + route, headers=headers, json=req_body)
-    if r is None:  # handle errors
-        # raise OperationError
-        pass"""
