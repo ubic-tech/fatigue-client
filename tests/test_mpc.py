9818d12d1b468c38aa6b8b9f0128702c44b347a8
armnov <arm.nov@ya.ru>
2020-09-04 01:32:47 +0300

    shares_len -> shares_count

--- tests/test_mpc.py
+++ tests/test_mpc.py
@@ -39,59 +39,59 @@ def get_request_data(hash_ids: List[str], shares_count: int)\
     ]
 
 
-def continue_mpc_validator(request_data, self_db_data, shares_len, processed_request_data, for_ubic):
+def continue_mpc_validator(request_data, self_db_data, shares_count, processed_request_data, for_ubic):
     assert len(for_ubic) == len(processed_request_data)
 
     for r, p, u in zip(request_data, processed_request_data, for_ubic):
         assert p.hash_id == u.hash_id == r.hash_id
-        assert len(p.shares) == len(u.shares) == len(r.shares) == shares_len
+        assert len(p.shares) == len(u.shares) == len(r.shares) == shares_count
         hash_id = p.hash_id
         db_shares = self_db_data[hash_id]
-        assert shares_len == len(db_shares)
-        for i in range(shares_len):
+        assert shares_count == len(db_shares)
+        for i in range(shares_count):
             web_part = u.shares[i] + p.shares[i]
             db_part = r.shares[i] + db_shares[i]
             assert web_part == db_part
 
 
-def continue_mpc_helper(request_data, self_db_data, shares_len):
+def continue_mpc_helper(request_data, self_db_data, shares_count):
     processed_request_data = deepcopy(request_data)
     for_ubic = continue_mpc(processed_request_data, self_db_data)
     continue_mpc_validator(request_data,
                            self_db_data,
-                           shares_len,
+                           shares_count,
                            processed_request_data,
                            for_ubic)
 
 
-def finalize_mpc_validator(request_data, self_db_data, shares_len, processed_request_data):
+def finalize_mpc_validator(request_data, self_db_data, shares_count, processed_request_data):
     for r, p in zip(request_data, processed_request_data):
         assert r.hash_id == p.hash_id
         hash_id = r.hash_id
         db_shares = self_db_data[hash_id]
-        assert len(db_shares) == len(r.shares) == len(p.shares) == shares_len
-        for i in range(shares_len):
+        assert len(db_shares) == len(r.shares) == len(p.shares) == shares_count
+        for i in range(shares_count):
             assert db_shares[i] + r.shares[i] == p.shares[i]
 
 
-def finalize_mpc_helper(request_data, self_db_data, shares_len):
+def finalize_mpc_helper(request_data, self_db_data, shares_count):
     processed_request_data = deepcopy(request_data)
     finalize_mpc(processed_request_data, self_db_data)
-    finalize_mpc_validator(request_data, self_db_data, shares_len, processed_request_data)
+    finalize_mpc_validator(request_data, self_db_data, shares_count, processed_request_data)
 
 
 def test_mpc():
     endpoint_hash_ids = ["", "a", ]
     for self_db_data in self_db_data_sets:
-        shares_len = len(self_db_data[drivers_hash_ids[0]])  # lens should be equal
-        request_data = get_request_data(drivers_hash_ids, shares_len)
+        shares_count = len(self_db_data[drivers_hash_ids[0]])  # lens should be equal
+        request_data = get_request_data(drivers_hash_ids, shares_count)
 
-        continue_mpc_helper(request_data, self_db_data, shares_len)
-        finalize_mpc_helper(request_data, self_db_data, shares_len)
+        continue_mpc_helper(request_data, self_db_data, shares_count)
+        finalize_mpc_helper(request_data, self_db_data, shares_count)
         for endpoint_hash_id in endpoint_hash_ids:
             original_request_data = deepcopy(request_data)
             for_ubic, request_data = mpc_strategy(request_data, self_db_data, endpoint_hash_id)
             if endpoint_hash_id:
-                continue_mpc_validator(original_request_data, self_db_data, shares_len, request_data, for_ubic)
+                continue_mpc_validator(original_request_data, self_db_data, shares_count, request_data, for_ubic)
             else:
-                finalize_mpc_validator(original_request_data, self_db_data, shares_len, request_data)
+                finalize_mpc_validator(original_request_data, self_db_data, shares_count, request_data)
