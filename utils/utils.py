8bdb8b58fb7d1006810633fd2e04eca522015295
anovikov <anovikov@ubic.tech>
2020-09-08 22:06:53 +0300

    TMPC-19 no re

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,4 @@
-from re import search, findall, sub
-from datetime import datetime, date, time
+from datetime import datetime
 
 import aiohttp
 
@@ -21,20 +20,4 @@ async def request(url, *, method='post', expected_status=200, **kwargs):
 
 
 def timestamp_to_datetime(timestamp: str) -> datetime:
-    pattern = "{year}-{month}-{day}T{hour}:{minute}:{second}Z"
-
-    # re stuff
-    regex = sub(r'{(.+?)}', r'(?P<_\1>.+)', pattern)
-    values = list(search(regex, timestamp).groups())
-    keys = findall(r'{(.+?)}', pattern)
-
-    #  values to int
-    for i in range(len(values)):
-        values[i] = int(values[i])
-
-    _dict = dict(zip(keys, values))
-    d = date(_dict["year"], _dict["month"], _dict["day"])
-    t = time(_dict["hour"], _dict["minute"], _dict["second"])
-
-    return datetime.combine(d, t)
-    # return datetime.strptime('%d-%m-%yT%H:%M:%SZ')
+    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
