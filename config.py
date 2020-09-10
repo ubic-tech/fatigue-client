bdd9fefcf702f004e29cab874897fabdd871d084
anovikov <anovikov@ubic.tech>
2020-09-10 20:13:52 +0300

    TMPC-29 default AGGR_UUID changed

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class AggregatorConfig:
     ENDPOINTS_ROUTE: str = "/v1/endpoints"
     ENDPOINTS_TTL = 3600
     AGGR_NAME: str = "Yandex"
-    AGGR_UUID: str = "8704d129-1af0-489e-b761-d40344c12e70"
+    AGGR_UUID: str = "777aaaaa-1af0-489e-b761-d40344c12e70"
     CLICK_HOUSE_URL: str = "archy1.dc"
