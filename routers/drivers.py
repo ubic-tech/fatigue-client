61aef61a5818967c1857c9c1b00e1a787b8ae729
armnov <arm.nov@ya.ru>
2020-09-13 17:30:57 +0300

    TMPC-19 no data_extractor

--- routers/drivers.py
+++ routers/drivers.py
@@ -3,10 +3,12 @@ from uuid import UUID
 from fastapi import Header, APIRouter, Request, BackgroundTasks
 from pydantic.error_wrappers import ValidationError
 from aiocache import cached
+from typing import Iterable, List
 
 from utils.utils import request, OperationError
 from models import drivers, common
 from repository.clickhouse_repository import ClickhouseRepository
+from repository.drivers_repository import DriverID
 from core.mpc import continue_mpc, finalize_mpc
 from config import AggregatorConfig as AggrConf
 
@@ -47,32 +49,24 @@ def get_next_endpoint_uuid(chain: drivers.List[UUID], my_uuid: str):
         return None
 
 
-async def process(x_request_id, req_body, path, data_extractor):
+async def process(x_request_id, req_body, path, my_data):
     """
     organizes strategy of MPC and web request forwarding
     :param x_request_id: header from request to be forwarded
     :param req_body: request's data body (JSON is expected)
     :param path: url specifying the MPC destination
-    :param data_extractor: data extraction method appropriate for
+    :param my_data: data extraction method appropriate for
         current MPC destination
     """
     headers = {"X-Request-Id": x_request_id, }
-    ubic_shares_route = AggrConf.UBIC_URL + AggrConf.SHARES_ROUTE
-    start = req_body.start
-    drivers_hash_ids = [d.hash_id for d in req_body.drivers]
-    if req_body.end:
-        my_data = data_extractor(drivers_hash_ids, start, req_body.end)
-    else:
-        my_data = data_extractor(drivers_hash_ids, start)
 
-    chain = req_body.chain
-    if next_endpoint_uuid := get_next_endpoint_uuid(chain,
+    if next_endpoint_uuid := get_next_endpoint_uuid(req_body.chain,
                                                     AggrConf.AGGR_UUID):
         next_endpoint = await get_endpoint_by_uuid(next_endpoint_uuid)
         for_ubic, for_next_aggr = continue_mpc(req_body.drivers, my_data)
-        ctrl_body = drivers.ControlBody(start=start,
+        ctrl_body = drivers.ControlBody(start=req_body.start,
                                         end=req_body.end,
-                                        chain=chain,
+                                        chain=req_body.chain,
                                         drivers=for_next_aggr)
         r = await request(next_endpoint + path, headers=headers, data=ctrl_body.json())
         shares_body = drivers.SharesBody(next=UUID(next_endpoint_uuid), drivers=for_ubic)
@@ -82,9 +76,15 @@ async def process(x_request_id, req_body, path, data_extractor):
         shares_body = drivers.SharesBody(drivers=for_ubic)
 
     if r == common.SUCCESS:
-        await request(ubic_shares_route, headers=headers, data=shares_body.json())
+        await request(AggrConf.UBIC_URL + AggrConf.SHARES_ROUTE,
+                      headers=headers,
+                      data=shares_body.json())
 
     return common.SUCCESS
+
+
+def get_hash_ids(drivers_shares: List[drivers.DriverShares]
+                 ) -> Iterable[DriverID]: return [d.hash_id for d in drivers_shares]
 
 
 @router.post("/drivers/fatigue",
@@ -100,14 +100,17 @@ def fatigue(raw_request: Request,
              response_model=common.StatusResponse,
              response_model_exclude_unset=True)
 async def online_hourly(raw_request: Request,
-                        online_hourly_data: drivers.ControlBody,
+                        ctrl_body: drivers.ControlBody,
                         background_tasks: BackgroundTasks,
                         x_request_id: str = Header(...)):
     background_tasks.add_task(process,
                               x_request_id,
-                              online_hourly_data,
+                              ctrl_body,
                               raw_request.url.path,
-                              db.get_hourly)
+                              db.get_hourly(
+                                  get_hash_ids(ctrl_body.drivers),
+                                  ctrl_body.start)
+                              )
     return common.SUCCESS
 
 
@@ -115,14 +118,17 @@ async def online_hourly(raw_request: Request,
              response_model=common.StatusResponse,
              response_model_exclude_unset=True)
 async def history_hourly(raw_request: Request,
-                         online_history_data: drivers.ControlBody,
+                         ctrl_body: drivers.ControlBody,
                          background_tasks: BackgroundTasks,
                          x_request_id: str = Header(...)):
     background_tasks.add_task(process,
                               x_request_id,
-                              online_history_data,
+                              ctrl_body,
                               raw_request.url.path,
-                              db.get_history_hourly)
+                              db.get_history_hourly(
+                                  get_hash_ids(ctrl_body.drivers),
+                                  ctrl_body.start)
+                              )
     return common.SUCCESS
 
 
@@ -130,14 +136,17 @@ async def history_hourly(raw_request: Request,
              response_model=common.StatusResponse,
              response_model_exclude_unset=True)
 async def online_quarter_hourly(raw_request: Request,
-                                online_quarter_hourly_data: drivers.ControlBody,
+                                ctrl_body: drivers.ControlBody,
                                 background_tasks: BackgroundTasks,
                                 x_request_id: str = Header(...)):
     background_tasks.add_task(process,
                               x_request_id,
-                              online_quarter_hourly_data,
+                              ctrl_body,
                               raw_request.url.path,
-                              db.get_quarter_hourly)
+                              db.get_quarter_hourly(
+                                  get_hash_ids(ctrl_body.drivers),
+                                  ctrl_body.start)
+                              )
     return common.SUCCESS
 
 
@@ -145,12 +154,16 @@ async def online_quarter_hourly(raw_request: Request,
              response_model=common.StatusResponse,
              response_model_exclude_unset=True)
 async def on_order(raw_request: Request,
-                   on_order_data: drivers.ControlBody,
+                   ctrl_body: drivers.ControlBody,
                    background_tasks: BackgroundTasks,
                    x_request_id: str = Header(...)):
     background_tasks.add_task(process,
                               x_request_id,
-                              on_order_data,
+                              ctrl_body,
                               raw_request.url.path,
-                              db.get_on_order)
+                              db.get_on_order(
+                                  get_hash_ids(ctrl_body.drivers),
+                                  ctrl_body.start,
+                                  ctrl_body.end)
+                              )
     return common.SUCCESS
