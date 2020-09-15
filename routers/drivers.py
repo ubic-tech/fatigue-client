6e7f1b1c70d8a41414a799d306f57ea086641eeb
anovikov <anovikov@ubic.tech>
2020-09-15 14:01:21 +0300

    TMPC-19 chain interating

--- routers/drivers.py
+++ routers/drivers.py
@@ -18,9 +18,9 @@ db = ClickhouseRepository(AggrConf.CLICK_HOUSE_URL,
 
 
 @cached(ttl=AggrConf.ENDPOINTS_TTL)
-async def get_endpoint_by_uuid(uuid) -> str:
+async def get_endpoint_by_uuid(uuid: UUID) -> str:
     route = AggrConf.UBIC_URL + AggrConf.ENDPOINTS_ROUTE
-    endpoints_request = drivers.EndpointsBody(identifiers=[UUID(uuid)])
+    endpoints_request = drivers.EndpointsBody(identifiers=[uuid, ])
     resp = await request(route, data=endpoints_request.json())
     try:
         return drivers.EndpointResponse(**resp).endpoints[0].endpoint
@@ -28,24 +28,17 @@ async def get_endpoint_by_uuid(uuid) -> str:
         raise OperationError
 
 
-def get_next_endpoint_uuid(chain: drivers.List[UUID], my_uuid: str):
-    """
-    Pops AggrConf.AGGR_HASH_ID from the chain
-    the 1st hash ID is expected to be AggrConf.AGGR_UUID
-        raises OperationError if not
-    :param chain: list of endpoints' hash IDs
-    :param my_uuid: this aggregator's hash ID
-    :return: hash ID of an endpoint following after AggrConf.AGGR_UUID
-        or an empty string if does not exist
-    """
-    try:  # the 1st uuid is expected to be 'mine' and should be popped out
-        if str(chain.pop(0)) != my_uuid:
-            raise OperationError
-    except IndexError:
+def check_if_mine_uuid_1st_and_pop(chain: drivers.List[UUID], mine: UUID):
+    try:
+        assert chain.pop(0) == mine
+    except (IndexError, AssertionError):
         raise OperationError
-    try:  # try getting next aggr in chain
-        return str(chain[0])  # return the next endpoint's uuid
-    except IndexError:  # means 'I' am the last aggregator in the chain
+
+
+def get_next_endpoint_uuid(chain: drivers.List[UUID]):
+    try:
+        return chain[0]
+    except IndexError:
         return None
 
 
@@ -59,11 +52,8 @@ async def process(x_request_id, req_body, path, my_data):
         current MPC destination
     """
     headers = {"X-Request-Id": x_request_id, }
-    next_endpoint_uuid = get_next_endpoint_uuid(req_body.chain,
-                                                AggrConf.AGGR_UUID)
-
     # if no next endpoint simply send 'my' data += ctrl_body's to Ubic
-    if not next_endpoint_uuid:
+    if not (next_endpoint_uuid := get_next_endpoint_uuid(req_body.chain)):
         for_ubic = finalize_mpc(req_body.drivers, my_data)
         shares_body = drivers.SharesBody(drivers=for_ubic)
         await request(AggrConf.UBIC_URL + AggrConf.SHARES_ROUTE,
@@ -71,23 +61,23 @@ async def process(x_request_id, req_body, path, my_data):
                       data=shares_body.json())
         return
 
+    # if 'I' am not the last 'I' should continue MPC
     for_ubic, for_next_aggr = continue_mpc(req_body.drivers, my_data)
     ctrl_body = drivers.ControlBody(start=req_body.start,
                                     end=req_body.end,
                                     chain=req_body.chain,
                                     drivers=for_next_aggr)
     while next_endpoint_uuid:
+        # keep trying to get a response from any of next endpoints
         next_endpoint = await get_endpoint_by_uuid(next_endpoint_uuid)
-
-        # keep trying to get any next endpoint's response
         r = await request(next_endpoint + path, headers=headers, data=ctrl_body.json())
         if common.StatusResponse(**r) != common.SUCCESS:
-            next_endpoint_uuid = get_next_endpoint_uuid(req_body.chain,
-                                                        AggrConf.AGGR_UUID)
+            req_body.chain.pop(0)  # pop unavailable endpoint
+            next_endpoint_uuid = get_next_endpoint_uuid(req_body.chain)
             continue
 
         #  if next responded ok I can send my shares to Ubic
-        shares_body = drivers.SharesBody(next=UUID(next_endpoint_uuid), drivers=for_ubic)
+        shares_body = drivers.SharesBody(next=next_endpoint_uuid, drivers=for_ubic)
         r = await request(AggrConf.UBIC_URL + AggrConf.SHARES_ROUTE,
                           headers=headers,
                           data=shares_body.json())
@@ -116,6 +106,7 @@ async def online_hourly(raw_request: Request,
                         ctrl_body: drivers.ControlBody,
                         background_tasks: BackgroundTasks,
                         x_request_id: str = Header(...)):
+    check_if_mine_uuid_1st_and_pop(ctrl_body.chain, UUID(AggrConf.AGGR_UUID))
     background_tasks.add_task(process,
                               x_request_id,
                               ctrl_body,
@@ -134,6 +125,7 @@ async def history_hourly(raw_request: Request,
                          ctrl_body: drivers.ControlBody,
                          background_tasks: BackgroundTasks,
                          x_request_id: str = Header(...)):
+    check_if_mine_uuid_1st_and_pop(ctrl_body.chain, UUID(AggrConf.AGGR_UUID))
     background_tasks.add_task(process,
                               x_request_id,
                               ctrl_body,
@@ -152,6 +144,7 @@ async def online_quarter_hourly(raw_request: Request,
                                 ctrl_body: drivers.ControlBody,
                                 background_tasks: BackgroundTasks,
                                 x_request_id: str = Header(...)):
+    check_if_mine_uuid_1st_and_pop(ctrl_body.chain, UUID(AggrConf.AGGR_UUID))
     background_tasks.add_task(process,
                               x_request_id,
                               ctrl_body,
@@ -170,6 +163,7 @@ async def on_order(raw_request: Request,
                    ctrl_body: drivers.ControlBody,
                    background_tasks: BackgroundTasks,
                    x_request_id: str = Header(...)):
+    check_if_mine_uuid_1st_and_pop(ctrl_body.chain, UUID(AggrConf.AGGR_UUID))
     background_tasks.add_task(process,
                               x_request_id,
                               ctrl_body,
