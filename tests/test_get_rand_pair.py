bb1ec77f5a56a27ad95c0ef3fe6dfe26425cf3f9
Peter Emelianov <pemelyanov@ubic.tech>
2020-09-19 21:19:09 +0300

    fixed test_random_pair

--- tests/test_get_rand_pair.py
+++ tests/test_get_rand_pair.py
@@ -1,21 +1,22 @@
 from hypothesis import given
 import hypothesis.strategies as st
 
-from core.mpc import get_rand_pair, MODULO
+from core.mpc import get_rand_pair
+from config import AggregatorConfig
 
 
-@given(st.integers(min_value=0, max_value=MODULO - 1))
+@given(st.integers(min_value=0, max_value=AggregatorConfig.MODULO - 1))
 def test_get_rand_pair_positive(secret: int):
     share1, share2 = get_rand_pair(secret)
-    print(share1, share2, secret, (share1 + share2) % MODULO)
-    assert (share1 + share2) % MODULO == secret
+    print(share1, share2, secret, (share1 + share2) % AggregatorConfig.MODULO)
+    assert (share1 + share2) % AggregatorConfig.MODULO == secret
 
 
-@given(st.integers(min_value=MODULO))
+@given(st.integers(min_value=AggregatorConfig.MODULO))
 def test_get_rand_pair_negative(secret: int):
     try:
         _, _ = get_rand_pair(secret)
     except AssertionError:
         assert True
         return
-    assert False, f"if {secret} greater {MODULO} AssertionError is expected"
+    assert False, f"if {secret} greater {AggregatorConfig.MODULO} AssertionError is expected"
